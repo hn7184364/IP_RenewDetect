342835eae035a8462b04e9bb5134694dcc95d316
Kyo <Kyo@15-CK022TX>
2018-07-08 10:59:07 +0800

    Service build successful

---
+++ IP_RenewDetect/IP_RenewDetect.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace IP_RenewDetect
+{
+    partial class IP_RenewDetect
+    {
+        /// <summary> 
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary> 
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // IP_RenewDetect
+            // 
+            this.ServiceName = "IP_RenewDetect";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ IP_RenewDetect/IP_RenewDetect.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace IP_RenewDetect
+{
+    public partial class IP_RenewDetect : ServiceBase
+    {
+        string currentIP = "";
+
+        public IP_RenewDetect()
+        {
+            InitializeComponent();
+            
+            eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("IP_RenewDetect"))
+            {
+                EventLog.CreateEventSource(
+                    "IP_RenewDetect",null);
+            }
+            eventLog1.Source = "IP_RenewDetect";
+            //eventLog1.Log = "IP_Log";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Set up a timer to trigger every minute.
+            Timer timer1 = new Timer();
+            timer1.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer1.Interval = 60000; // 60 seconds  
+            timer1.Start();
+        }
+
+        private int eventId = 1;
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            string sURL;
+            sURL = "http://ifconfig.io/ip/";
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+            String ResIP = objReader.ReadLine();
+            // Console.WriteLine("***" + ResIP + "***");
+            if (!ResIP.Equals(this.currentIP))
+            {
+                // Console.WriteLine("IP updated");
+                currentIP = ResIP;
+                eventLog1.WriteEntry("Current IP is "+currentIP, EventLogEntryType.Information, eventId++);
+            }
+            
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Stop();
+            eventLog1.WriteEntry("Stop IP_RenewDetect service");
+        }
+    }
+}
